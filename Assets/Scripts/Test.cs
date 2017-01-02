9b90d29be28b3bddc5cec0bfd01d33ab425c77da
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-01-01 22:11:28 -0500

    Added spawner, rotationlerp and a timer
    Pingpong is doomed

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    Juice juice = new Juice();
+    BasicJuice juice = new BasicJuice();
+    BasicTimer timer = new BasicTimer();
+    public Transform newPosition;
+    public float testTimer = 5;
+
+    void Start()
+    {
+        timer.timerLeft = testTimer;
+    }
 
     void Update()
     {
-        juice.PingPongLerpObjectHorizontal(gameObject, 1f);
+        juice.GameObjectLerping("LerpObjectVector2", gameObject, newPosition);
+
+        timer.CountDownFrom(testTimer);
+
+        print(timer.timerLeft);
+
+        if (timer.timerLeft == 0)
+        {
+            print("Time's up!");
+        }
     }
 }
