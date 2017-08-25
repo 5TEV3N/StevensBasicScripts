5f09a2917265a99988971dfec7c9ff29292371ba
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-08-25 14:55:47 -0400

    REDUX TIME

---
+++ Assets/Scripts/Camera/FirstPersonCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    private Camera cam;                                              // Reff to the camera
+    public GameObject player;                                        // Tag your player before using this script!!!!
+
+    public float mouseSensitivity;
+    public float upDownRange = 90.0f;                                // How far i can look up or down.
+    private float verticalRotation = 0;                              // Contains the MouseYAxis
+    private float mouseXAxis;
+    private float mouseYAxis;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        this.transform.SetParent(player.transform);
+        this.transform.position = player.transform.position;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        mouseXAxis = Input.GetAxis("Mouse X");
+        mouseYAxis = Input.GetAxis("Mouse Y");
+
+        if (mouseXAxis != 0 || mouseYAxis != 0)
+        {
+            Mouselook(mouseXAxis, mouseYAxis);
+        }
+    }
+
+    public void Mouselook(float mouseXAxis, float mouseYAxis)
+    {
+        //Filter Horizontal input
+        if (mouseXAxis != 0)
+        {
+            player.transform.Rotate(new Vector3(0, mouseXAxis, 0));
+
+        }
+
+        //Filter Vertical input
+        if (mouseYAxis != 0)
+        {
+            verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+            verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+            cam.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header ("Player")]
+    public Sprite playerSprite;
+
+    [Header("Values")]
+    public bool firstPersonController;
+    public float playerSpeed;
+    public float currentVelocity;
+    public float maxVelocity;
+
+    [Header ("")]
+    public float playerJumpHeight;               // jump Height 
+    public float maxTerminalVelocity;            // max speed of how fast the player falls
+    public int jumpsAllowed;                     // how many jumps you can perform
+    public int jumpCounter;                      // keeps track of how many legal jumps you performed
+    public float jumpRayDistance;                // how long the ray is 
+
+    private Rigidbody rb;
+    private float xAxis = 0f;
+    private float zAxis = 0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        currentVelocity = rb.velocity.magnitude;
+    }
+
+    private void FixedUpdate()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        zAxis = Input.GetAxisRaw("Vertical");
+
+        if (firstPersonController == false)
+        {
+            if (xAxis != 0)
+            {
+                PlayerMove(xAxis, 0, firstPersonController);
+            }
+        }
+        else if (xAxis != 0 || zAxis != 0)
+        {
+            PlayerMove(xAxis, zAxis, firstPersonController);
+        }
+    }
+
+    public void PlayerMove(float xAxis, float zAxis, bool FirstPerson)
+    {
+        if (FirstPerson == false)
+        {
+            if (xAxis < 0)
+            {
+                //playerSprite.flipX = true;
+                if (Mathf.Abs(currentVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(-transform.right * playerSpeed, ForceMode.Impulse);
+                }
+            }
+            if (xAxis > 0)
+            {
+                //playerSprite.flipX = false;
+                if (Mathf.Abs(currentVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(transform.right * playerSpeed, ForceMode.Impulse);
+                }
+            }
+        }
+        else
+        {
+            if (xAxis < 0)
+            {
+                //playerSprite.flipX = true;
+                if (Mathf.Abs(currentVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(-transform.right * playerSpeed, ForceMode.Impulse);
+                }
+            }
+            if (xAxis > 0)
+            {
+                //playerSprite.flipX = false;
+                if (Mathf.Abs(currentVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(transform.right * playerSpeed, ForceMode.Impulse);
+                }
+            }
+
+            if (zAxis < 0)
+            {
+                if (Mathf.Abs(currentVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(-transform.forward * playerSpeed, ForceMode.Impulse);
+                }
+            }
+            if (zAxis > 0)
+            {
+                if (Mathf.Abs(currentVelocity) <= maxVelocity)
+                {
+                    rb.AddForce(transform.forward * playerSpeed, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
