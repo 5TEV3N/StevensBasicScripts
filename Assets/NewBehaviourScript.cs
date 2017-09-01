a86207eaa52604c4248ffb5c1cb8f2bb71c1d435
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-09-01 19:16:49 -0400

    Testing some shit out

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Spawner spawn = new Spawner();
+    public GameObject bullet;
+
+    private void Start()
+    {
+        InvokeRepeating("ShootingTheBullet", 1.0f, 0.1f);
+    }
+    private void ShootingTheBullet()
+    {
+        spawn.SpawnObjectAtSpot(gameObject.transform, bullet);
+    }
+}
---
+++ Assets/bulletscript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletscript : MonoBehaviour {
+    Rigidbody rb;
+	// Update is called once per frame
+	void Update ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up, ForceMode.Impulse);
+	}
+}
